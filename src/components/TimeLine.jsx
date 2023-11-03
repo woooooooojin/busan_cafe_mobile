96e8443928848e8bb498b1f015ec984fa73232df
woooooooojin <kevin2205@naver.com>
2023-11-03 12:50:38 +0900

    timeline ui

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,15 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { collection, disableNetwork, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { auth, db } from '../firebase';
-import { Unsubscribe } from "firebase/auth";
+import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { db } from '../firebase';
 
 
 
 const Wrapper = styled.div`
     display: flex;
-    gap: 10px;
+    gap: 15px;
     flex-direction: column;
+    padding: 10px;
+    margin-bottom: 50px;
+
+    .wrap{
+        border: 1px solid #A6C1E8;
+        border-radius: 10px;
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+    }
+    .text_wrap{
+        width: 70%;
+        .username{
+            font-weight: 500;
+            font-size: 18px;
+        }
+        .post_desc{
+            margin-top: 5px;
+        }
+    }
+    .img_wrap{
+        width: 30%;
+        text-align: center;
+        img{
+            width: 70px;
+            height: 70px;
+        }
+    }
 `
 
 export default function TimeLine() {
@@ -19,12 +46,12 @@ export default function TimeLine() {
 
         const fetchPosts = async()=>{
             const postQuery = query(
-                collection(db,'posts'), //어떤 컬ㅔ션을 쿼하 싶은지 firestore 인스턴스를 매개변수로 넘겨줌
+                collection(db,'posts'), //어떤 컬렉션을 쿼리하고 싶은지 firestore 인스턴스를 매개변수로 넘겨줌
                 orderBy('createdAt',"desc"), //createdAt 기준으로 내림차순
                 limit(25)
             )
             
-                 unsubscribe = await onSnapshot(postQuery, (snapshot)=>{
+                unsubscribe = await onSnapshot(postQuery, (snapshot)=>{
                 const posting = snapshot.docs.map((doc)=>{
                     const {post, createdAt, userId, username, photo} = doc.data()
                     return{
@@ -39,7 +66,7 @@ export default function TimeLine() {
         return()=>{
             unsubscribe && unsubscribe()
         }   
-    },[])
+    },[]) 
 
    
 
@@ -49,9 +76,15 @@ export default function TimeLine() {
             
                  {
                     post.map((item) => (
-                    <div key={item.id}>
-                      <p>Username: {item.username}</p>
-                      <p>Post: {item.post}</p>
+                    <div className='wrap' key={item.id}>
+                        <div className="text_wrap">
+                            <span className='username'>{item.username}</span>
+                            <p className='post_desc'>{item.post}</p>
+                        </div>
+
+                        <div className="img_wrap">
+                            {item.photo ? <img src={item.photo} alt="img" /> : null}
+                        </div>
                     </div>
                   ))
                 }
