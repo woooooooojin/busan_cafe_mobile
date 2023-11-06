b4bd533c7087e31828f738d665866659d84f31b0
woooooooojin <kevin2205@naver.com>
2023-11-06 11:35:40 +0900

    delete post

--- src/components/TimeLine.jsx
+++ src/components/TimeLine.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
-import { db } from '../firebase';
+import { collection, deleteDoc, doc, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { auth, db, storage } from '../firebase';
+import { deleteObject, ref } from 'firebase/storage';
 
 
 
@@ -18,6 +19,7 @@ const Wrapper = styled.div`
         padding: 20px;
         display: flex;
         justify-content: center;
+        position: relative;
     }
     .text_wrap{
         width: 70%;
@@ -37,10 +39,29 @@ const Wrapper = styled.div`
             height: 70px;
         }
     }
+
+    .del_btn{
+        position: absolute;
+        right: 5px;
+        top: 5px;
+        z-index: 99;
+        cursor: pointer;
+    }
+`
+const DeleteBtn = styled.button`
+    width: 25px;
+    height: 25px;
+    background-color: transparent;
+    border-radius: 50%;
+    font-size: 12px;
+    border: 1px solid tomato;
+    color: tomato;
 `
 
 export default function TimeLine() {
     const [post, setPost] = useState([])
+    const user = auth.currentUser
+
     useEffect(()=>{
         let unsubscribe = null
 
@@ -68,15 +89,41 @@ export default function TimeLine() {
         }   
     },[]) 
 
+    const onDelete = async(item)=>{
+        const ok = window.confirm('삭제하시겠습니까?')
+       
+        if(!ok)return
+    
+       try{
+        //doc함수를 사용하여 db에서 포스트를 찾고 deleteDoc함수를 입력해주어야 한다
+        //doc 함수 인자 : db, document 이름, id
+            await deleteDoc(doc(db,'posts',item.id))
+            
+
+            if(item.photo){
+                const photoRef = ref(storage, `posts/${user.uid}/${item.id}`) //storage 사진의 경로
+                await deleteObject(photoRef)
+            }
+       
+       }catch(e){
+        console.log(e)
+       }
+       
+    }
+
    
 
   return (
     <>
         <Wrapper>
             
+            
                  {
                     post.map((item) => (
                     <div className='wrap' key={item.id}>
+                        
+                        <DeleteBtn className='del_btn' onClick={() => onDelete(item)}>X</DeleteBtn>
+
                         <div className="text_wrap">
                             <span className='username'>{item.username}</span>
                             <p className='post_desc'>{item.post}</p>
