03f87d10a7848358009a06023f927a24c5ec278d
woooooooojin <kevin2205@naver.com>
2023-11-09 10:36:04 +0900

    title

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -3,12 +3,34 @@ import Header from './Header'
 import Footer from './Footer'
 import PostForm from './PostForm'
 import TimeLine from './TimeLine'
+import styled from 'styled-components'
+
+const Title = styled.h2`
+  margin-top: 10px;
+  text-align: center;
+  letter-spacing: 5px;
+  color: black;
+  position: relative;
+
+  &::before,
+  &::after {
+    content: '[';
+    display: inline-block;
+    position: relative;
+    top: 1px;
+    height: 100%;
+    color: steelblue;
+    font-size: 30px;
+  }
+
+  &::after { content: ']'; }
+`
 
 export default function Community() {
   return (
     <>
       <Header/>
-        
+      <Title>Community</Title>
       <PostForm/>
       <TimeLine/>
       <Footer/>
