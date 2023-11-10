0deb42470d7be45a069b90819a1d96e1f98404a5
woooooooojin <kevin2205@naver.com>
2023-11-10 10:01:39 +0900

    profile / edit name

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -33,6 +33,7 @@ const ProfileInput = styled.input`
 `
 const Name = styled.span`
     font-size: 22px;
+    text-align: center;
 `
 
 const LogBtn = styled.button`
@@ -41,12 +42,35 @@ const LogBtn = styled.button`
     border-radius: 10px;
     border: 1px solid #ccc;
 `
-
+const EditName = styled.button`
+    font-size: 12px;
+    background-color: transparent;
+    padding: 2px 5px;
+    border: 1px solid steelblue;
+    border-radius: 5px;
+    color: steelblue;
+    cursor: pointer;
+    display: block;
+    margin: 5px auto;
+`
+const NameInput = styled.input`
+    display: block;
+    width: 150px;
+    margin: 0 auto;
+    height: 25px;
+    outline: none;
+    border: 2px solid steelblue;
+    border-radius: 20px;
+    padding: 0 10px;
+`
 export default function Profile() {
 
     const navigate = useNavigate()
     const user = auth.currentUser
-    const [profile, setProfile] = useState(user?.photoURL)
+    const [profile, setProfile] = useState(user.photoURL)
+
+    const [editName, setEditName] = useState(false) //edit name state
+    const [name, setName] = useState(user.displayName ?? '사용자')
 
     const onProfileChange = async (e)=>{
         const {files} = e.target;
@@ -55,7 +79,7 @@ export default function Profile() {
 
         if(files && files.length === 1){
             const file = files[0]
-            const locationRef = ref(storage,`profile/${user?.uid}`)
+            const locationRef = ref(storage,`profile/${user.uid}`)
             const result = await uploadBytes(locationRef,file)
             const avatarUrl = await getDownloadURL(result.ref) 
             setProfile(avatarUrl)
@@ -76,6 +100,26 @@ export default function Profile() {
         navigate('/login')
     }
 
+    const onChangeName = (e)=>{
+        setName(e.target.value)
+    } //name input change event
+
+    const onEditNameClick = async()=>{
+        if(!user) return
+        setEditName(true)
+        if(!editName) return
+
+        try{
+            await updateProfile(user,{
+                displayName : name,
+            })
+        }catch(e){
+            console.log(e)
+        }finally{
+            setEditName(false)
+        }
+    }//name change
+
   return (
     <>
         <Header/>
@@ -91,7 +135,9 @@ export default function Profile() {
             <ProfileInput onChange={onProfileChange} id='avatar' accept='image/*' type='file' />
             
             <Name>
-                {user.displayName ? user.displayName : 'anonymous'}
+                {user.displayName ? user.displayName : '사용자'}
+                {editName ? <NameInput onChange={onChangeName} type='text' value={name}/> : null}
+                <EditName onClick={onEditNameClick}>{editName ? "저장" : "수정"}</EditName>
             </Name>
 
 
