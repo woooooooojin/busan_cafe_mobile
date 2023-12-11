c1b1b08dbfc340f2b23d9bc0457649ea9c5f45ed
woooooooojin <kevin2205@naver.com>
2023-12-11 10:39:05 +0900

    css

--- src/pages/Logopage.jsx
+++ src/pages/Logopage.jsx
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 
 const LogoBox = styled.div`
     width: 100%;
-    height: 100vh;
+    height: calc(100vh - 50px);
     display: flex;
     flex-direction: column;
     justify-content: space-around;
