52906385b57a9a7173dd50908c72938a38f7efe8
woooooooojin <kevin2205@naver.com>
2023-12-11 10:39:55 +0900

    css

--- src/pages/Logopage.jsx
+++ src/pages/Logopage.jsx
@@ -85,16 +85,17 @@ return (
     <div>
         <LogoBox>
             <AttentionSeeker tada>
+                
                 <h1>Caffeine</h1>
 
                 <div className="logo_wrap">
                     <img src={ process.env.PUBLIC_URL + '/img/cafe_icon.png'} alt="img"/>
                 </div>
 
-                
+                <p>ⓒ 2023. Woooooooojin all rights reserved.</p>
 
             </AttentionSeeker>
-            <p>ⓒ 2023. Woooooooojin all rights reserved.</p>
+            
         </LogoBox>
 
 
